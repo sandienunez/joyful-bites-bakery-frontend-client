2c7a60fe10592f99bdd41ca00c764ae60d5484c0
sandienunez <github email address>
2021-01-23 07:11:03 -0500

    Refactor LikeButton Component by adding useState hook to increment and decrement number of likes that client makes, Set the initial count state to zero, create increment and decrement event handlers using setCount, add buttons for client to give a like or thumbs down for the MenuItems component

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,62 +1,27 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import '../LikeButton.css';
-
+import React, { useState } from "react";
  
-class LikeButton extends Component {
-    constructor() {
-        super();
-        this.state = {
-          liked: false
-        };
-        this.handleClick = this.handleClick.bind(this);
-      } 
-      
-      handleClick() {
-        this.setState({
-          liked: !this.state.liked
-        });
-      }
-      
-      render() {
-        const label = this.state.liked ? '♡' : '❤️'
-        return (
-          <div className="customContainer">
-            <button className="btn btn-primary" onClick={this.handleClick}>
-              {label}</button>
+function LikeButton() {
+  const [likesCounted, setCount] = useState(0);
 
-          </div>
-        );
-      }
-    
+  const incrementMe = () => {
+    setCount(prevCount => prevCount + 1);
+  };
 
-    // state = {
-    //     like: 0,
-    //     dislike: 0
-    // }
-
-    // incrementMe = () => {
-    //     let newCount = this.state.like + 1
-    //     this.setState({
-    //       like: newCount
-    //     })
-    //   }
-
-    //   decrementMe = () => {
-    //     let newCount = this.state.dislike + 1
-    //     this.setState({
-    //       dislike: newCount
-    //     })
-    //   }
-
-    // render() { 
-    //     return (
-    //         <div>
-    //             <button onClick={this.incrementMe}> ❤️ Like: {this.state.like} </button>
-    //             <button onClick={this.decrementMe}> 👎 Dislike: {this.state.dislike} </button>
-
-    //         </div>
-    //     );
-    // }
+  const decrementMe = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+  return (
+    <div>
+        <div>
+      <button onClick={incrementMe}>❤️</button>
+      <button onClick={decrementMe}>👎</button>
+          <h5> {likesCounted} likes</h5>
+        </div>
+    </div>
+  );
 }
- 
+
+
 export default LikeButton;
