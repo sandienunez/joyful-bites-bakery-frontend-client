8210d073e20988573261eafd5af061c715849fb1
sandienunez <github email address>
2021-04-11 23:51:18 -0400

    Add header tag to OurStory Component

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -9,6 +9,7 @@ const OurStory = () => {
         <div className="Orders">
             <h2>Our Story</h2>
             <h4>We are a team of joyful bakers ready to set the world on fire with healthy, nutritious and delicious treats! </h4>
+            <h4>Joyful Bites was born from the idea of using healhy whole food in the baked goods that makes people feel good. </h4>
             <img src={cookiemonster} alt="cookiemonster" height={300} width={300}/>
 
         </div>
