7bb1f14d4692353c62d0def1f4e8f2a8614d6dec
sandienunez <github email address>
2021-05-23 23:50:31 -0400

    Add header tag to contactus component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -11,6 +11,8 @@ const ContactUs = () => {
          <img src={pink} alt="pink" height={550} width={1400}/>
             <h5>❤️ Please email us for any baking inquiries, special orders or events.</h5>
              <h5>🍪 We offer catering services for up to 500 guests. </h5>
+             <h5>🍪 Below you will find our contact info </h5>
+
             <h5>📍 Address: 1213 4th St. NW, Washington D.C. </h5>
             <h5>✉️ Email: joyfulbitesbakery@gmail.com</h5>
             <h5>☎️ Phone Number: 111-111-1111</h5>
